89eaab99541414d40e56e1b57e338c22ee04956d
raulibinceanu-ship-it <raulibinceanu@gmail.com>
2025-12-03 17:17:09 +0100

    last fixings

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,38 +1,24 @@
-import { Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addToFavourites } from "../redux/actions";
+import { Link } from "react-router-dom";
 
 const Job = ({ data }) => {
   const dispatch = useDispatch();
 
   return (
     <div className="job-card">
-      <a href={`/${data.company_name}`}>{data.company_name}</a>
+      <Link to={`/${data.company_name}`}>{data.company_name}</Link>
 
-      <button onClick={() => dispatch(addToFavourites(data.company_name))}>
-        ⭐ Aggiungi ai preferiti
+      <button
+        onClick={() => dispatch(addToFavourites(data.company_name))}
+        style={{ marginLeft: "10px" }}
+      >
+        Aggiungi ai preferiti
       </button>
 
       <h3>{data.title}</h3>
     </div>
   );
 };
-
-const Job = ({ data }) => (
-  <Row
-    className="mx-0 mt-3 p-3"
-    style={{ border: "1px solid #00000033", borderRadius: 4 }}
-  >
-    <Col xs={3}>
-      <Link to={`/${data.company_name}`}>{data.company_name}</Link>
-    </Col>
-    <Col xs={9}>
-      <a href={data.url} target="_blank" rel="noreferrer">
-        {data.title}
-      </a>
-    </Col>
-  </Row>
-);
 
 export default Job;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-
-createRoot(document.getElementById("root")).render(<App />);
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import App from "./App.jsx";
+import "./index.css";
+
+const root = createRoot(document.getElementById("root")); // ⬅️ MANCAVA QUESTO
 
 root.render(
   <Provider store={store}>
